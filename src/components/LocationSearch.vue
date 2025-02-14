b90d9864e06f275217200e4d02beb63301aea52a
Carifio24 <carifio24@gmail.com>
2025-02-14 12:37:53 -0500

    Combobox location search is working, need to prevent WWT movement.

--- src/components/LocationSearch.vue
+++ src/components/LocationSearch.vue
@@ -3,61 +3,63 @@
     class="forward-geocoding-container"
     :style="cssStyles"
   >
-    <div
-      class="forward-geocoding-input-row"
+    <v-combobox
+      v-show="searchOpen"
+      :class="['forward-geocoding-input', locationJustUpdated ? 'geocode-success' : '', small ? 'forward-geocoding-input-small' : '']"
+      v-model="searchText"
+      :items="searchResults ? searchResults.features : []"
+      :item-title="itemText"
+      :bg-color="bgColor"
+      label="Enter city or zip"
+      :density="small ? 'compact' : 'default'"
+      hide-details
+      solo
+      :color="accentColor"
+      @input="() => {}"
+      @keydown.enter="performForwardGeocodingSearch"
+      @keydown.esc="searchResults = null"
+      :error-messages="searchErrorMessage"
+      @click:append="focusCombobox"
+      @update:focused="onFocusChange($event)"
+      ref="search-input"
+      :menu="menuOpen"
+      @update:menu="menuOpen = $event"
     >
-      <v-text-field
-        v-show="searchOpen"
-        v-model="searchText"
-        :class="['forward-geocoding-input', locationJustUpdated ? 'geocode-success' : '', small ? 'forward-geocoding-input-small' : '']"
-        :label="locationJustUpdated ? 'Location Updated' : 'Enter a location'"
-        bg-color='black'
-        density="compact"
-        hide-details
-        variant="solo"
-        :color="accentColor"
-        @keydown.stop
-        @keyup.enter="() => performForwardGeocodingSearch()"
-        @keyup.esc="searchResults = null"
-        @click:clear="searchResults = null"
-        :error-messages="searchErrorMessage"
-      ></v-text-field>
+    <template v-slot:append>
       <font-awesome-icon
         class="geocoding-search-icon"
         icon="magnifying-glass"
         :size="searchOpen ? 'xl' : buttonSize"
-        :color="!searchOpen || (searchText && searchText.length > 2) ? accentColor : 'gray'"
-        @click="() => {
-          if (searchOpen) {
-            performForwardGeocodingSearch();
-          } else {
-            searchOpen = true;
-          }
-        }"
+        color="gray"
+        @click="toggleSearch"
       ></font-awesome-icon>
-      
-    </div>
-    
-    <div
-      class="forward-geocoding-results"
-      :class="[small ? 'results-small' : '']"
-      v-if="searchResults !== null"
-    >
-      <div
-        v-for="(feature, index) in (searchResults !== null ?  searchResults.features : [])"
-        class="forward-geocoding-result"
-        :key="index"
-        @click="() => setLocationFromSearchFeature(feature)"
-      >
-        {{ feature.place_name }}
-      </div>
-    </div>
+
+      <slot name="append-icon" class="geocode-icon"></slot>
+
+      <font-awesome-icon
+        class="geocoding-close-icon"
+        v-show="searchOpen && !stayOpen"
+        icon="circle-xmark"
+        :size="searchOpen ? 'xl' : '1x'"
+        color="gray"
+        @click="closeSearch"
+      ></font-awesome-icon>
+    </template>
+  </v-combobox>
+  <font-awesome-icon
+      v-show="!searchOpen && !stayOpen"
+      class="geocoding-search-icon"
+      icon="magnifying-glass"
+      :size="searchOpen ? 'xl' : buttonSize"
+      color="gray"
+      @click.prevent="toggleSearch"
+    ></font-awesome-icon>
   </div>
 </template> 
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { MapBoxFeature, MapBoxFeatureCollection } from "@cosmicds/vue-toolkit/src/mapbox";
+import { computed, ref, useTemplateRef } from "vue";
+import { MapBoxFeature, MapBoxFeatureCollection, textForMapboxFeature } from "@cosmicds/vue-toolkit/src/mapbox";
 import { watch } from "vue";
 
 type SearchProvider = (searchText: string) => Promise<MapBoxFeatureCollection | null>;
@@ -70,6 +72,7 @@ export interface LocationSearchProps {
   small?: boolean;
   theme?: string;
   buttonSize?: string;
+  bgColor?: string;
 }
 
 const props = withDefaults(defineProps<LocationSearchProps>(), {
@@ -80,7 +83,9 @@ const props = withDefaults(defineProps<LocationSearchProps>(), {
   small: false,
   theme: "dark",
   buttonSize: "1x",
+  bgColor: "black",
 });
+console.log(props);
 
 const emit = defineEmits<{
   (e: "error", message: string): void
@@ -93,15 +98,24 @@ const searchText = ref<string | null>(null);
 const searchResults = ref<MapBoxFeatureCollection | null>(null);
 const searchErrorMessage = ref<string | null>(null);
 const locationJustUpdated = ref(false);
+const menuOpen = ref(false);
+const comboFocused = ref(false);
 
 const cssStyles = computed(() => {
   return {
     "--accent-color": props.accentColor,
-    "--bg-color": "black",
+    "--bg-color": props.bgColor,
     "--fg-container-padding": searchOpen.value ? (props.small ? "0px 5px 0px 0px" : "5px 10px 12px 10px") : "0px",
     "--border-radius": searchOpen.value ? "7px" : "20px",
   };
 });
+
+function itemText(item: string | MapBoxFeature): string {
+  if (typeof item === "string") {
+    return item;
+  }
+  return textForMapboxFeature(item);
+}
 
 function performForwardGeocodingSearch() {
   const search = searchText.value;
@@ -121,7 +135,37 @@ function performForwardGeocodingSearch() {
   });
 }
 
+function focusCombobox() {
+  const input = useTemplateRef("search-input").value as HTMLInputElement;
+  input.focus();
+}
+
+function blurCombobox() {
+  const input = useTemplateRef("search-input").value as HTMLInputElement;
+  input.blur();
+}
+
+function toggleSearch() {
+  if (searchOpen.value) {
+    performForwardGeocodingSearch();
+    menuOpen.value = true;
+    focusCombobox();
+  } else {
+    searchOpen.value = true;
+  }
+}
+
+function closeSearch() {
+  searchOpen.value = false;
+  clearSearchData();
+}
+
+function onFocusChange(focused: boolean) {
+  comboFocused.value = focused;
+}
+
 function setLocationFromSearchFeature(feature: MapBoxFeature) {
+  blurCombobox();
   timedJustUpdatedLocation();
   clearSearchData();
   emit("set-location", feature);
@@ -155,26 +199,47 @@ watch(searchText, function(text: string | null) {
 </script>
 
 <style lang="less">
+
+// https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors
 .forward-geocoding-container {
-  width: fit-content;
+  --border-radius: 20px;
+  position: relative;
   color: var(--accent-color);
-  background-color: black;
   border: 2px solid var(--accent-color);
-  border-radius: 7px;
+  border-radius: var(--border-radius);
+  background-color: var(--bg-color);
+  margin-left: 0.25rem;
+  margin-bottom: 0.25rem;
   padding: var(--fg-container-padding);
 
+  // there are two separate labels, we want the 2nd one to be large. the first is the small floating label
+  .v-field > .v-field__field > .v-label.v-field-label:nth-child(2) {
+    font-size: 1.2rem;
+  }
+
+  // .v-input--horizontal .v-input__append {
+  //   margin-inline-start: 0;
+  // }
+  
   .v-text-field {
     min-width: 150px;
-    width: min(200px, 20vw);
   }
+  
+  .v-field--variant-filled.v-field--has-background .v-field__overlay {
+    border-top-right-radius: 0px;
+}
 
   .forward-geocoding-input > .v-input__control > .v-field {
     border-radius: var(--border-radius);
   }
   
   .forward-geocoding-input.geocode-success label {
-    color: var(--accent-color);
     opacity: 1;
+  }
+  
+  .forward-geocoding-input-small label {
+    // .v-label sets default to 1rem
+    font-size: 0.8rem;
   }
 
   .forward-geocoding-input-row {
@@ -184,46 +249,15 @@ watch(searchText, function(text: string | null) {
     gap: 10px;
     align-items: center;
   }
-
+  
   .geocoding-search-icon {
     padding-inline: calc(0.3 * var(--default-line-height));
     padding-block: calc(0.4 * var(--default-line-height));
   }
 
-  .geocoding-search-icon:hover, .geocoding-close-icon:hover {
+  .geocoding-search-icon:hover, #geocoding-close-icon:hover {
     cursor: pointer;
   }
 
-  // For some reason setting width: 100% makes the search results 2px too small
-  // It's probably some Vuetify styling thing
-  // Maybe there's a better workaround, but this gets the job done for now
-  .forward-geocoding-results {
-    position: absolute;
-    top: 42px;
-    left: -1px;
-    width: calc(100% + 2px);
-    background: black;
-    border: 2px solid var(--accent-color);
-    border-top: 0px;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-    padding: 0px 10px;
-
-    &.results-small {
-      top: 37px;
-      width: calc(100% + 4px);
-      left: -2px;
-    }
-
-    .forward-geocoding-result {
-      border-top: 2px solid var(--accent-color);
-      font-size: 12pt;
-      pointer-events: auto;
-
-      &:hover {
-        cursor: pointer;
-      }
-    }
-  }
 }
 </style>
